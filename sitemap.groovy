921d47ff6db449d4d04b35e827850917502270da
Guillaume Laforge <glaforge@gmail.com>
2014-06-26 15:31:31 +0200

    formatting and fixed some documentation links

--- sitemap.groovy
+++ sitemap.groovy
@@ -1,46 +1,46 @@
 documentation {
     section('Getting started','fa-graduation-cap') {
-        item 'Download Groovy', 'download', 'download'
-        item 'Install Groovy', 'install', 'install'
-        item 'Differences with Java', 'differences', 'core-differences'
-        item 'The Groovy Development Kit', 'gdk', 'core-gdk'
-        item 'Runtime and compile-time metaprogramming', 'metaprogramming', 'core-metaprogramming'
-        item 'The Grape dependency manager', 'grapes', 'core-grapes'
-        item 'Testing guide', 'testing', 'core-testing'
-        item 'Domain-Specific Languages', 'dsls', 'core-dsls'
-        item 'Security', 'security', 'security'
-        item 'Design patterns in Groovy', 'design-patterns', 'core-design-patterns'
+        item 'Download Groovy',                             'download',         'download'
+        item 'Install Groovy',                              'install',          'install'
+        item 'Differences with Java',                       'differences',      'core-differences-java'
+        item 'The Groovy Development Kit',                  'gdk',              'core-gdk'
+        item 'Runtime and compile-time metaprogramming',    'metaprogramming',  'core-metaprogramming'
+        item 'The Grape dependency manager',                'grapes',           'core-grapes'
+        item 'Testing guide',                               'testing',          'core-testing-guide'
+        item 'Domain-Specific Languages',                   'dsls',             'core-domain-specific-languages'
+        item 'Security',                                    'security',         'security'
+        item 'Design patterns in Groovy',                   'design-patterns',  'design-pattern-in-groovy'
     }
 
     section('Groovy module guides', 'fa-cubes') {
-        item 'Parsing and producing JSON', 'json', 'json'
-        item 'Working with a relational database', 'databases', 'databases'
-        item 'Processing XML', 'processing-xml', 'processing-xml'
-        item 'Scripting Ant tasks', 'scripting-ant', 'scripting-ant'
-        item 'Template engines', 'templating', 'template-engines'
-        item 'Creating Swing UIs', 'swing', 'swing'
-        item 'Servlet support', 'servlet', 'servlet'
-        item 'Working with JMX', 'jmx', 'jmx'
+        item 'Parsing and producing JSON',          'json',             'json-userguide'
+        item 'Working with a relational database',  'databases',        'databases'
+        item 'Processing XML',                      'processing-xml',   'processing-xml'
+        item 'Scripting Ant tasks',                 'scripting-ant',    'scripting-ant'
+        item 'Template engines',                    'templating',       'template-engines'
+        item 'Creating Swing UIs',                  'swing',            'swing'
+        item 'Servlet support',                     'servlet',          'servlet'
+        item 'Working with JMX',                    'jmx',              'jmx'
     }
 
     section ('Tools','fa-gears') {
-        item 'groovyc — the Groovy compiler', 'groovyc', 'tools-groovyc'
-        item 'groovysh — the Groovy command -like shell', 'groovysh', 'tools-groovysh'
-        item 'groovyConsole — the Groovy Swing console', 'groovyconsole', 'tools-groovyconsole'
+        item 'groovyc — the Groovy compiler',               'groovyc',          'tools-groovyc'
+        item 'groovysh — the Groovy command -like shell',   'groovysh',         'tools-groovysh'
+        item 'groovyConsole — the Groovy Swing console',    'groovyconsole',    'tools-groovyconsole'
         item 'IDE integration', 'ide', 'tools-ide'
     }
 
     section ('Language Specification', 'fa-graduation-cap') {
-        item 'Syntax', 'syntax', 'core-syntax'
-        item 'Operators', 'operators', 'core-operators'
-        item 'Program structure', 'structure', 'core-program-structure'
-        item 'Object orientation', 'objectorientation', 'core-object-orientation'
-        item 'Closures', 'closures', 'core-closures'
-        item 'Semantics', 'semantics', 'core-semantics'
+        item 'Syntax',              'syntax',               'core-syntax'
+        item 'Operators',           'operators',            'core-operators'
+        item 'Program structure',   'structure',            'core-program-structure'
+        item 'Object orientation',  'objectorientation',    'core-object-orientation'
+        item 'Closures',            'closures',             'core-closures'
+        item 'Semantics',           'semantics',            'core-semantics'
     }
 
     section ('API documentation', 'fa-code') {
-        item 'GroovyDoc documentation of the Groovy APIs', 'groovydoc', 'groovydoc'
-        item 'The Groovy Development Kit enhancements', 'gdk', 'gdk'
+        item 'GroovyDoc documentation of the Groovy APIs',  'groovydoc',    'groovydoc'
+        item 'The Groovy Development Kit enhancements',     'gdk',          'gdk'
     }
 }
---
+++ src/site/pages/download.groovy
@@ -0,0 +1,27 @@
+layout 'layouts/main.groovy',
+        pageTitle: 'The Groovy programming language - Download',
+        contents: contents {
+            div(id: 'content', class: 'page-1') {
+                div(class: 'row') {
+                    div(class: 'row-fluid') {
+                        div(class: 'col-lg-3') {
+                            ul(class: 'nav-sidebar') {
+                                li(class: 'active') {
+                                    a(href: 'download.html') { strong('Download') }
+                                }
+                            }
+                        }
+
+                        div(class: 'col-lg-8 col-lg-pull-0') {
+                            h1('Download')
+                            p {
+
+
+                            }
+                            hr(class: 'divider')
+
+                        }
+                    }
+                }
+            }
+        }

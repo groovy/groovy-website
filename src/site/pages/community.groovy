6318a1ad363233172f2cbc6670cb6c7c80007971
Guillaume Laforge <glaforge@gmail.com>
2014-06-28 14:29:20 +0200

    Empty community page for now (we'll see if we still need it, considering we have the "socialize" aspect already)

---
+++ src/site/pages/community.groovy
@@ -0,0 +1,26 @@
+layout 'layouts/main.groovy',
+        menu: menu,
+        pageTitle: 'The Groovy programming language - Download',
+        contents: contents {
+            div(id: 'content', class: 'page-1') {
+                div(class: 'row') {
+                    div(class: 'row-fluid') {
+                        div(class: 'col-lg-3') {
+                            ul(class: 'nav-sidebar') {
+                                li(class: 'active') {
+                                    a(href: 'community.html') { strong('Community') }
+                                }
+                            }
+                        }
+
+                        div(class: 'col-lg-8 col-lg-pull-0') {
+                            h1 'Community'
+                            article {
+
+                            }
+                            hr(class: 'divider')
+                        }
+                    }
+                }
+            }
+        }

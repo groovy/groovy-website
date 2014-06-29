87ea76634bb50e7ead733681eb27cc61f2cba26e
Guillaume Laforge <glaforge@gmail.com>
2014-06-29 10:56:33 +0200

    couple of questions for the FAQ

--- src/site/pages/faq.groovy
+++ src/site/pages/faq.groovy
@@ -15,10 +15,24 @@ layout 'layouts/main.groovy',
 
                         div(class: 'col-lg-8 col-lg-pull-0') {
                             h1 'FAQ - Frequently Asked Questions'
-                            article {
-
-                            }
                             hr(class: 'divider')
+                            article {
+                                h2 'Is Groovy an Open Source project?'
+                                p {
+                                    yield 'Yes, Groovy is an Open Source programming language project, licensed under the '
+                                    a(href: 'http://www.apache.org/licenses/LICENSE-2.0', 'Apache License v2')
+                                    yield '. You can see the license header in all the source files of the project, as well as a '
+                                    a(href: 'https://github.com/groovy/groovy-core/blob/master/LICENSE.txt', 'license file')
+                                    yield ' at the root of the project'
+                                }
+
+                                h2 'What are the differences between Groovy and Java?'
+                                p {
+                                    yield 'Learn more about the '
+                                    a(href: 'differences.html', 'differences between Groovy and Java.')
+                                    yield '.'
+                                }
+                            }
                         }
                     }
                 }

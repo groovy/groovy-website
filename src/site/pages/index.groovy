99e56d3987342b98075aa21e51351671fc4e7717
Guillaume Laforge <glaforge@gmail.com>
2014-07-01 23:20:05 +0200

    just keep the first three events in the front page, so that events can be shown in full only in the events page

--- src/site/pages/index.groovy
+++ src/site/pages/index.groovy
@@ -9,30 +9,23 @@ layout 'layouts/main.groovy',
             }
             div(id: 'content') {
                 include unescaped: 'html/index.html'
-//                include unescaped: 'html/next-events.html'
 
-                boolean titleDisplayed = false
-                allEvents.keySet().toList().collate(3).each { List<String> eventNames ->
-                    section(class: "row colset-3-article") {
-                        if (!titleDisplayed) {
-                            h1 { strong "Groovy events you shouldn't miss!" }
-                            titleDisplayed = true
-                        }
-                        eventNames.each { String eventName ->
-                            Event event = allEvents[eventName]
-                            article {
-                                div(class: 'content') {
-                                    div(class: 'event-img', style: "background-image: url(${event.logo})") {}
-                                    h1 {
-                                        a(href: event.url) {
-                                            strong eventName
-                                            br()
-                                            em event.location
-                                        }
+                section(class: "row colset-3-article") {
+                    h1 { strong "Groovy events you shouldn't miss!" }
+                    allEvents.keySet().take(3).each { String eventName ->
+                        Event event = allEvents[eventName]
+                        article {
+                            div(class: 'content') {
+                                div(class: 'event-img', style: "background-image: url(${event.logo})") {}
+                                h1 {
+                                    a(href: event.url) {
+                                        strong eventName
+                                        br()
+                                        em event.location
                                     }
-                                    time event.date
-                                    yieldUnescaped event.description
                                 }
+                                time event.date
+                                yieldUnescaped event.description
                             }
                         }
                     }

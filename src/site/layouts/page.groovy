b71747fd901cde7af08fca725abb19026fa60565
Guillaume Laforge <glaforge@gmail.com>
2014-07-04 10:18:33 +0200

    Add Google Analytics to see which pages / sections are the most interesting to visitors

--- src/site/layouts/page.groovy
+++ src/site/layouts/page.groovy
@@ -76,6 +76,16 @@ html {
         ['vendor/jquery-1.10.2.min.js', 'vendor/classie.js', 'vendor/sidebarEffects.js', 'plugins.js', 'main.js', *scripts].each {
             yieldUnescaped "<script src='js/$it'></script>"
         }
+
+        script '''
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-257558-10', 'auto');
+              ga('send', 'pageview');
+        '''
     }
 }
 

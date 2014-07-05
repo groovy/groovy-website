5b6aab3f48139f152f12241f266f7aa7721e256e
Guillaume Laforge <glaforge@gmail.com>
2014-07-05 11:03:27 +0200

    same title as second nav label

--- src/site/pages/versioning.groovy
+++ src/site/pages/versioning.groovy
@@ -35,7 +35,7 @@ layout 'layouts/main.groovy',
                         }
 
                         div(class: 'col-lg-8 col-lg-pull-0') {
-                            h1 'The Groovy version scheme'
+                            h1 'Groovy version scheme'
                             article {
                                 h2 'Since Groovy 2.0.0'
                                 p {

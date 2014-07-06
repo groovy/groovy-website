094c4b1e69f5475c8c043ee5e8a3040b79e79206
Guillaume Laforge <glaforge@gmail.com>
2014-07-06 14:29:53 +0200

    rename encosystem "entry"'s to "project"

--- buildSrc/src/main/groovy/Ecosystem.groovy
+++ buildSrc/src/main/groovy/Ecosystem.groovy
@@ -1,5 +1,5 @@
 class Ecosystem extends LinkedHashMap<String,EcosystemEntry> {
-    void entry(String name, Closure entrySpec) {
+    void project(String name, Closure entrySpec) {
         def entry = new EcosystemEntry(name:name)
         def clone = entrySpec.rehydrate(entry,entry,entry)
         clone()

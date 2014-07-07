b29aac5f915af0985482079dc5391dad442902e8
Guillaume Laforge <glaforge@gmail.com>
2014-07-07 12:17:56 +0200

    update github page link after the refactor of the project in two modules

--- site/src/site/includes/contribute-button.groovy
+++ site/src/site/includes/contribute-button.groovy
@@ -1,6 +1,6 @@
 div(id: 'contribute-btn') {
     button(type: 'button', class: 'btn btn-default',
-            onclick: "window.location.href=\"https://github.com/groovy/groovy-website/blob/master/src/site/pages/${currentPage}.groovy\"") {
+            onclick: "window.location.href=\"https://github.com/groovy/groovy-website/tree/master/site/src/site/pages/${currentPage}.groovy\"") {
         i(class: 'fa fa-pencil-square-o') {}
         yield ' Improve this doc'
     }

aadd473f8cf838b64a34f5647ef1ed8bae04ebcb
Guillaume Laforge <glaforge@gmail.com>
2014-07-08 00:25:25 +0200

    adding version 2.3.3 in the list of documentation to browse

--- site/src/site/pages/documentation.groovy
+++ site/src/site/pages/documentation.groovy
@@ -67,7 +67,7 @@ layout 'layouts/main.groovy', true,
                                     '2.0.0', '2.0.1', '2.0.2', '2.0.3', '2.0.4', '2.0.5', '2.0.6', '2.0.7', '2.0.8',
                                     '2.1.0', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.1.5', '2.1.6', '2.1.7', '2.1.8', '2.1.9',
                                     '2.2.0', '2.2.1', '2.2.2',
-                                    '2.3.0', '2.3.1', '2.3.2', 'Select a version'
+                                    '2.3.0', '2.3.1', '2.3.2', '2.3.3', 'Select a version'
                             ].reverse()
 
                             select(class: 'form-control', onchange: "window.location.href='http://docs.groovy-lang.org/docs/groovy-' + this.value + '/html/'") {

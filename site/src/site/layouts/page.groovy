bd1c9a7f6cfc7ef31fa1ace13a7c701662125bdc
Guillaume Laforge <glaforge@gmail.com>
2014-07-15 14:58:18 +0200

    use a device-width viewport

--- site/src/site/layouts/page.groovy
+++ site/src/site/layouts/page.groovy
@@ -19,6 +19,7 @@ html {
     head {
         meta charset: 'utf-8'
         meta 'http-equiv': 'X-UA-Compatible', content: 'IE=edge'
+        meta name: 'viewport', content: "width=device-width, initial-scale=1"
         title(pageTitle)
         link(href: "img/favicon.ico", type: "image/x-ico", rel: "icon")
         def styles = extraStyles ?: []

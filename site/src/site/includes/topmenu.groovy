530c0b74e2d1726d9accc65044a514010ca9c5ef
Guillaume Laforge <glaforge@gmail.com>
2014-07-17 23:33:26 +0200

    Upgrade to bootstrap 2.3.2 Use the navbar menu from bootstrap Minimizes changes to bootstrap.css to its minimum (ie. use a min-width media query of 1000px instead of 768px for collapsing the menu) Put custom style changes to style.css only.

--- site/src/site/includes/topmenu.groovy
+++ site/src/site/includes/topmenu.groovy
@@ -1,3 +1,4 @@
+/*
 header(id: 'header') {
     div(class: 'row') {
         h1(id: 'logo') { a(href: 'index.html', 'Groovy') }
@@ -18,3 +19,68 @@ header(id: 'header') {
         }
     }
 }
+*/
+
+div(class: 'navbar navbar-default navbar-static-top', role: 'navigation') {
+    div(class: 'container') {
+        div(class: 'navbar-header') {
+            button(type: 'button', class: 'navbar-toggle', 'data-toggle': 'collapse', 'data-target': '.navbar-collapse') {
+                span(class: 'sr-only') {}
+                span(class: 'icon-bar') {}
+                span(class: 'icon-bar') {}
+                span(class: 'icon-bar') {}
+            }
+            a(class: 'navbar-brand', href: 'index.html') {
+                i(class: 'fa fa-star') {}
+                yield ' Groovy'
+            }
+        }
+        div(class: 'navbar-collapse collapse') {
+            ul(class: 'nav navbar-nav') {
+                menu['Groovy'].each { menuItem ->
+                    li(class: category == menuItem.name ? 'active' : '') { a(href: menuItem.link, menuItem.name) }
+                }
+            }
+            ul(class: 'nav navbar-nav navbar-right') {
+                li {
+                    a('data-effect': 'st-effect-9', class: 'st-trigger', href: '#', 'Socialize')
+                }
+                li(class: (category == 'Search') ? 'active' : '') {
+                    a(href: 'search.html') {
+                        i(class: 'fa fa-search') {}
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><i class="fa fa-star"></i> Groovy</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="learn.html">Learn</a></li>
+                <li><a href="documentation.html">Documentation</a></li>
+                <li><a href="download.html">Download</a></li>
+                <li><a href="community.html">Community</a></li>
+                <li><a href="ecosystem.html">Ecosystem</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../navbar/">Socialize</a></li>
+                <li><a href="search.html"><i class="fa fa-search"></i></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+ */
--- site/src/site/pages/documentation.groovy
+++ site/src/site/pages/documentation.groovy
@@ -56,16 +56,20 @@ layout 'layouts/main.groovy', true,
                                     }
                                 }
                             }
-                            hr(class: 'divider')
+                            div(class: 'col-md-12') {
+                                hr(class: 'divider')
 
-                            a(name: 'old-docs') {}
-                            h2 'Documentation for older versions'
-                            p 'You can browse the documentation of previous versions of Groovy since Groovy 1.7:'
-                            def allVersions =  [*allDocVersions, 'Select a version'].reverse()
+                                a(name: 'old-docs') {}
+                                article {
+                                    h2 'Documentation for older versions'
+                                    p 'You can browse the documentation of previous versions of Groovy since Groovy 1.7:'
+                                    def allVersions =  [*allDocVersions, 'Select a version'].reverse()
 
-                            select(class: 'form-control', onchange: "window.location.href='http://docs.groovy-lang.org/docs/groovy-' + this.value + '/html/'") {
-                                allVersions.each { String version ->
-                                    option version
+                                    select(class: 'form-control', onchange: "window.location.href='http://docs.groovy-lang.org/docs/groovy-' + this.value + '/html/'") {
+                                        allVersions.each { String version ->
+                                            option version
+                                        }
+                                    }
                                 }
                             }
                         }

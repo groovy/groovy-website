3dee0b2074730d8a1b8b21684c605a6285694620
Cedric Champeau <cedric.champeau@gmail.com>
2014-07-25 17:24:18 +0200

    Updated for Groovy 2.3.5

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -61,7 +61,7 @@ documentation {
             '2.0.0', '2.0.1', '2.0.2', '2.0.3', '2.0.4', '2.0.5', '2.0.6', '2.0.7', '2.0.8',
             '2.1.0', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.1.5', '2.1.6', '2.1.7', '2.1.8', '2.1.9',
             '2.2.0', '2.2.1', '2.2.2',
-            '2.3.0', '2.3.1', '2.3.2', '2.3.3', '2.3.4'])
+            '2.3.0', '2.3.1', '2.3.2', '2.3.3', '2.3.4','2.3.5'])
 
     section('Getting started','fa-graduation-cap') {
         //          NAME                                     TARGET HTML         DOCPAGE HTML                       GENERATE
@@ -129,10 +129,10 @@ downloads {
             yield ' of Groovy.'
         }
 
-        version('2.3.4') {
+        version('2.3.5') {
             stable true
-            releaseNotes 'https://jira.codehaus.org/secure/ReleaseNote.jspa?projectId=10242&version=20432'
-            windowsInstaller 'http://dist.codehaus.org/groovy/distributions/installers/windows/nsis/groovy-2.3.4-installer.exe'
+            releaseNotes 'https://jira.codehaus.org/secure/ReleaseNote.jspa?projectId=10242&version=20491'
+//            windowsInstaller 'http://dist.codehaus.org/groovy/distributions/installers/windows/nsis/groovy-2.3.4-installer.exe'
         }
     }
 }

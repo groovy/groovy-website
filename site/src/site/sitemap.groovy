ed8179ccd3e590f77dec3b38ab885372b4c4804d
Guillaume Laforge <glaforge@gmail.com>
2015-01-21 22:41:29 +0100

    Make 2.4.0 the latest official stable release

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -123,6 +123,7 @@ downloads {
         }
 
         version('2.4.0') {
+            stable true
             releaseNotes 'http://jira.codehaus.org/secure/ReleaseNote.jspa?projectId=10242&version=20921'
         }
     }
@@ -134,7 +135,6 @@ downloads {
         }
 
         version('2.3.9') {
-            stable true
             releaseNotes 'https://jira.codehaus.org/secure/ReleaseNote.jspa?projectId=10242&version=20788'
             windowsInstaller 'http://dl.bintray.com/groovy/Distributions/groovy-2.3.9-installer.exe'
         }

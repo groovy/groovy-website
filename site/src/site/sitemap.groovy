66a0f1c8c452471a652800462cf100343133d6fc
Keegan Witt <keeganwitt@gmail.com>
2015-10-18 16:35:03 -0400

    update Windows installer link

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -139,7 +139,7 @@ the ASF.</pre></p>
 
         version('2.4.5') {
             stable true
-            //windowsInstaller 'http://dl.bintray.com/groovy/Distributions/groovy-2.4.4-installer.exe'
+            windowsInstaller 'http://dl.bintray.com/groovy/Distributions/groovy-2.4.5-installer.exe'
         }
     }
 }

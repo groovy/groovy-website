7bf158dc9da9684244b96b5eadd5dc8381b13e66
Guillaume Laforge <glaforge@gmail.com>
2016-01-08 09:31:02 +0100

    Point at the @ApacheGroovy twitter account

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -22,7 +22,7 @@ menu {
     group('Socialize') {
         item 'Discuss on the mailing-list', 'mailing-lists.html',                               'fa-envelope'
         item 'Groovy Weekly newsletter',    'groovy-weekly.html',                               'fa-envelope-o'
-        item 'Groovy on Twitter',           'https://twitter.com/hashtag/groovylang',           'fa-twitter'
+        item 'Groovy on Twitter',           'https://twitter.com/ApacheGroovy',                 'fa-twitter'
         item 'Events and conferences',      'events.html',                                      'fa-calendar'
         item 'Source code on GitHub',       'https://github.com/apache/groovy',                 'fa-github'
         item 'Report issues in Jira',       'contribute.html#reporting-issues',                 'fa-bug'

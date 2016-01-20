05a5474a4318f061bf9fb7a42d6e09736b8db787
Guillaume Laforge <glaforge@gmail.com>
2016-01-20 09:49:58 +0100

    Remove old comments from the beta days

--- site/src/site/layouts/main.groovy
+++ site/src/site/layouts/main.groovy
@@ -12,19 +12,7 @@ layout 'layouts/page.groovy', true,
             // 'Content'
             div {
                 include template: 'includes/topmenu.groovy'
-/*
-                div(class:'alert alert-danger',role:'alert') {
-                    p '''You are seeing a beta version of the new Groovy website. You probably came here through the
-old Codehaus site which is currently down and beyond our control. This is a therefore a temporary redirection.
-'''
-                    p {
-                        yieldUnescaped 'While we do our best to restore the old version of the website, you can find '
-                        a(href:'documentation.html', 'a new documentation following this link.')
-                    }
-                    p '''We strongly encourage you to leverage this temporary workaround to give us your
-impressions about the new website and documentation. Thanks for your understanding!'''
-                }
-*/
+
                 // main contents goes here!
                 mainContent()
 

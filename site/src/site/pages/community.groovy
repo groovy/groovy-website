8b3a9b71ed04b749e4e7c9542e6b5694f0c189dd
Roberto Perez <roberto@perezalcolea.info>
2016-10-13 10:18:23 -0700

    Add Slack community link

--- site/src/site/pages/community.groovy
+++ site/src/site/pages/community.groovy
@@ -27,7 +27,7 @@ layout 'layouts/main.groovy', true,
                                 ul {
                                     li "how you can ${$a(href: 'contribute.html', 'contribute')} to the project, its codebase, its documentation,"
                                     li "how to raise issues in our ${$a(href: 'contribute.html#reporting-issues', 'bug tracker')},"
-                                    li "how to interact with other Groovy users and developers through the ${$a(href: 'mailing-lists.html', 'mailing-lists')},"
+                                    li "how to interact with other Groovy users and developers through the ${$a(href: 'mailing-lists.html', 'mailing-lists')} or ${$a(href: 'https://groovycommunity.com/', 'Slack')},"
                                     li "the upcoming ${$a(href: 'events.html', 'events and conferences')} you might want to attend to learn more about Groovy and to share your experience with others,"
                                     li "the list of ${$a(href: 'usergroups.html', 'user groups')} you can attend and where to meet other Groovy users."
                                 }

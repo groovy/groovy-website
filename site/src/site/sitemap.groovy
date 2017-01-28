297103dc1d03b53c46d3003ed945e7e7cfd32788
Virtualdogbert <virtualdogbert@gmail.com>
2017-01-28 10:27:11 -0500

    Adding Boston Groovy/Grails meetup to userGroups
    Just adding the Boston Groovy, Grails, Spring Meetup (B2GS) to the userGroup section.

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -354,6 +354,10 @@ usergroups {
         location 'North-America/United States'
         url 'http://www.meetup.com/Austin-Groovy-and-Grails-Users/'
     }
+    userGroup('Boston Groovy, Grails, Spring Meetup (B2GS)') {
+        location 'North-America/United States'
+        url 'https://www.meetup.com/Grails-Boston/'
+    }
     userGroup('Coder Consortium of Sacramento') {
         location 'North-America/United States'
         url 'http://coderconsortium.com/'

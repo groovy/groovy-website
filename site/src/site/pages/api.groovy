4f55c876c4d45801cc8c3c2fa748d9435056c47c
Guillaume Laforge <glaforge@gmail.com>
2017-03-24 23:28:09 +0100

    use Apache in the title tag

--- site/src/site/pages/api.groovy
+++ site/src/site/pages/api.groovy
@@ -1,2 +1,2 @@
 layout 'layouts/iframedoc.groovy', true,
-        pageTitle: "The Groovy programming language - Groovy Development Kit"
+        pageTitle: "The Apache Groovy programming language - Groovy Development Kit"

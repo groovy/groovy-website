192211df409821319c65e191183a3403c665b8e1
Guillaume Laforge <glaforge@gmail.com>
2017-03-24 23:30:07 +0100

    use Apache in the title tag

--- site/src/site/pages/documentation.groovy
+++ site/src/site/pages/documentation.groovy
@@ -1,5 +1,5 @@
 layout 'layouts/main.groovy', true,
-        pageTitle: 'The Groovy programming language - Documentation',
+        pageTitle: 'The Apache Groovy programming language - Documentation',
         mainContent: contents {
             div(id: 'content', class: 'page-1') {
                 div(class: 'row') {

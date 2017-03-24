1f0a5f0126d7d1b681ba22a5d2565ea3144fc017
Guillaume Laforge <glaforge@gmail.com>
2017-03-24 23:32:34 +0100

    use Apache in the title tag

--- site/src/site/pages/mailing-lists.groovy
+++ site/src/site/pages/mailing-lists.groovy
@@ -1,5 +1,5 @@
 layout 'layouts/main.groovy', true,
-        pageTitle: 'The Groovy programming language - Mailing-lists',
+        pageTitle: 'The Apache Groovy programming language - Mailing-lists',
         mainContent: contents {
             div(id: 'content', class: 'page-1') {
                 div(class: 'row') {

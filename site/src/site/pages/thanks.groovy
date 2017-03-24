06133ce3c0eb79fa6e610c68e4efd288ff1de828
Guillaume Laforge <glaforge@gmail.com>
2017-03-24 23:34:06 +0100

    use Apache in the title tag

--- site/src/site/pages/thanks.groovy
+++ site/src/site/pages/thanks.groovy
@@ -1,5 +1,5 @@
 layout 'layouts/main.groovy', true,
-        pageTitle: 'The Groovy programming language - Thanks',
+        pageTitle: 'The Apache Groovy programming language - Thanks',
         mainContent: contents {
             div(id: 'content', class: 'page-1') {
                 div(class: 'row') {

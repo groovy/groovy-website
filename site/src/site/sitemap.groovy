da6ba9b2f89c103102a6f8d0856de1ee31e560de
paulk <paulk@asert.com.au>
2017-06-24 22:29:03 +1000

    Release 2.4.12: tweak sitemap

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -67,7 +67,7 @@ documentation {
             '2.1.0', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.1.5', '2.1.6', '2.1.7', '2.1.8', '2.1.9',
             '2.2.0', '2.2.1', '2.2.2',
             '2.3.0', '2.3.1', '2.3.2', '2.3.3', '2.3.4', '2.3.5', '2.3.6', '2.3.7', '2.3.8', '2.3.9', '2.3.10', '2.3.11',
-            '2.4.0', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5', '2.4.6', '2.4.7', '2.4.8', '2.4.9', '2.4.10', '2.4.11',
+            '2.4.0', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5', '2.4.6', '2.4.7', '2.4.8', '2.4.9', '2.4.10', '2.4.11', '2.4.12',
             '2.5.0-alpha-1', '2.5.0-beta-1'
     ])
 
@@ -138,6 +138,10 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy.'
             yieldUnescaped ''' Important: Releases before 2.4.4 weren't done under the Apache Software Foundation and are provided as a convenience, without any warranty.'''
+        }
+        version('2.4.12') {
+            stable true
+            //windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-2.4.12-installer.exe'
         }
         version('2.4.11') {
             stable true

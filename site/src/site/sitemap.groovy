243974b09e25873cd74c65a6f9f077502ce0a8ff
paulk <paulk@asert.com.au>
2017-10-06 08:42:40 +1000

    amendments from release

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -68,8 +68,8 @@ documentation {
             '2.2.0', '2.2.1', '2.2.2',
             '2.3.0', '2.3.1', '2.3.2', '2.3.3', '2.3.4', '2.3.5', '2.3.6', '2.3.7', '2.3.8', '2.3.9', '2.3.10', '2.3.11',
             '2.4.0', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5', '2.4.6', '2.4.7', '2.4.8', '2.4.9', '2.4.10', '2.4.11', '2.4.12',
-            '2.5.0-alpha-1', '2.5.0-beta-1',
-            '2.6.0-alpha-1'
+            '2.5.0-alpha-1', '2.5.0-beta-1', '2.5.0-beta-2',
+            '2.6.0-alpha-1',
     ])
 
     section('Getting started','fa-graduation-cap') {
@@ -139,9 +139,9 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy. Pre-release versions are available:'
         }
-	version('2.5.0-beta-1') {
+	version('2.5.0-beta-2') {
             stable false
-            windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-2.5.0-beta-1-installer.exe'
+            //windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-2.5.0-beta-2-installer.exe'
         }
     }
     distribution('Groovy 2.4') {

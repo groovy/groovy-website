fcadc6bf924d28c9ca401562e350d6acefd8f111
Daniel Sun <sunlan@apache.org>
2017-11-16 01:09:41 +0000

    Add 2.6.0-alpha-2 windows installer link

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -130,7 +130,7 @@ downloads {
         }
         version('2.6.0-alpha-2') {
             stable false
-            // windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-2.6.0-alpha-2-installer.exe'
+            windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-2.6.0-alpha-2-installer.exe'
         }
     }
     distribution('Groovy 2.5') {

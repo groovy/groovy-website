e3668eff98e59cc4500930b398f3ba0d779b9b80
Daniel Sun <sunlan@apache.org>
2017-11-29 16:03:07 +0000

    Release 3.0.0-alpha-1

--- site/src/site/sitemap.groovy
+++ site/src/site/sitemap.groovy
@@ -69,7 +69,8 @@ documentation {
             '2.3.0', '2.3.1', '2.3.2', '2.3.3', '2.3.4', '2.3.5', '2.3.6', '2.3.7', '2.3.8', '2.3.9', '2.3.10', '2.3.11',
             '2.4.0', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5', '2.4.6', '2.4.7', '2.4.8', '2.4.9', '2.4.10', '2.4.11', '2.4.12', '2.4.13',
             '2.5.0-alpha-1', '2.5.0-beta-1', '2.5.0-beta-2',
-            '2.6.0-alpha-1', '2.6.0-alpha-2'
+            '2.6.0-alpha-1', '2.6.0-alpha-2', 
+            '3.0.0-alpha-1'
     ])
 
     section('Getting started','fa-graduation-cap') {
@@ -122,6 +123,17 @@ documentation {
 }
 
 downloads {
+    distribution('Groovy 3.0') {
+        description {
+            yield 'Groovy 3.0 is a bleeding edge '
+            a(href: 'versioning.html', 'version')
+            yield ' of Groovy designed for JDK8+ and with the new Parrot parser enabled by default. Pre-release versions are available:'
+        }
+        version('3.0.0-alpha-1') {
+            stable false
+            // windowsInstaller 'https://dl.bintray.com/groovy/Distributions/groovy-3.0.0-alpha-1-installer.exe'
+        }
+    }
     distribution('Groovy 2.6') {
         description {
             yield 'Groovy 2.6 is a bleeding edge '

eb1ca5584f02a017a06b8bb7f83d80cca0c5710c
Paul King <paulk@asert.com.au>
2018-10-19 11:46:36 +1000

    minor tweaks

--- site/gradle/templates/deadlinks.groovy
+++ site/gradle/templates/deadlinks.groovy
@@ -7,19 +7,17 @@ html {
         if (!deadLinks) {
             p("No dead link found. All green!")
         } else {
-            p "Found a total of ${deadLinks.values().sum { it.size()}} dead links."
-            p """Not that not all dead links can be fixed directly in the Groovy website project: some of them
-are generated in the Groovy documentation instead."""
-            p "The following files contain dead links:"
-        }
-        deadLinks.each { entry ->
-            def (page, links) = [entry.key, entry.value]
-            h3 { a(href:page,page) }
-            ul {
-                links.each { link ->
-                    asciidoc("At line ${link.line}, dead link to ${link.link}")
+            p "Found a total of ${deadLinks.values().sum{ it.size() }} dead links in the following files:"
+            deadLinks.each { entry ->
+                def (page, links) = [entry.key, entry.value]
+                h3 { a(href:page,page) }
+                ul {
+                    links.each { link ->
+                        li("At line ${link.line}, dead link to ${link.link}")
+                    }
                 }
             }
+            p "Note: not all dead links can be fixed directly in the Groovy website project - some are generated from the Groovy documentation and must be fixed there."
         }
     }
 }
